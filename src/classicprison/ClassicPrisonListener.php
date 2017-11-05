7af76d9080f331c36916c737b92cfb5ba4882a82
JackNoordhuis <Jack@CrazedCraftMC.net>
2017-11-05 20:53:15 +1100

    Add always spawn functionality

--- src/classicprison/ClassicPrisonListener.php
+++ src/classicprison/ClassicPrisonListener.php
@@ -23,6 +23,7 @@ use pocketmine\event\player\PlayerCreationEvent;
 use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerKickEvent;
+use pocketmine\event\player\PlayerLoginEvent;
 use pocketmine\event\player\PlayerQuitEvent;
 
 class ClassicPrisonListener implements Listener {
@@ -49,6 +50,18 @@ class ClassicPrisonListener implements Listener {
 	 */
 	public function onPlayerCreation(PlayerCreationEvent $event) {
 		$event->setPlayerClass(ClassicPrisonPlayer::class);
+	}
+
+	/**
+	 * @param PlayerLoginEvent $event
+	 *
+	 * @ignoreCancelled
+	 *
+	 * @priority MONITOR
+	 */
+	public function onLogin(PlayerLoginEvent $event) {
+		$pos = $this->getPlugin()->getServer()->getDefaultLevel()->getSafeSpawn();
+		$event->getPlayer()->teleport($pos->add(0.5, 0, 0.5));
 	}
 
 }
